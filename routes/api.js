d9addeb65e8a73199cb49a4df9424a582ae3c114
Sam <sammyknoxg@gmail.com>
2019-04-12 18:47:18 -0400

    updated dependencies

--- routes/api.js
+++ routes/api.js
@@ -24,6 +24,7 @@ module.exports = function(app) {
   });
 
   app.post('/scrape',function(req,res) {
+    req.setTimeout(500000);
 
     MLB.getAnalyticsForAdvantagePlayer(function(err, gameData) {
       console.log(err)
