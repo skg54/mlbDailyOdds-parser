610e1ab2fe6273a99ee36b1d8ea1ab108a06a499
Sam <sammyknoxg@gmail.com>
2019-04-12 11:30:57 -0400

    mlb game overunder added

--- models/MLB.js
+++ models/MLB.js
@@ -19,9 +19,7 @@ MLB.getAnalyticsForAdvantagePlayer = function(callback) {
     var dateObj = new Date();
 
     var month = ('0' + (dateObj.getMonth() + 1)).slice(-2);
-    // make date 2 digits
     var date = ('0' + dateObj.getDate()).slice(-2);
-    // get 4 digit year
     var year = dateObj.getFullYear();
     var dateForUrl = year + month + date;
 
@@ -113,78 +111,8 @@ MLB.getAnalyticsForAdvantagePlayer = function(callback) {
 
           return callback(null, gameArray);
         });
-        
       })
-    .catch(e => console.log(e));
-
-    // phantom.create().then(function(ph) {
-    //   ph.createPage().then(function(page) {
-    //     //page.property('customHeaders', {'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4'});
-
-    //     //page.settings.userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4';
-    //     //page.property('viewportSize', {width: 480, height: 800});
-    //     //page.property('viewportSize', {width: 1920, height: 1080});
-    //     _page = page;
-
-    //     _page.open(url).then(function(status) {
-    //       console.log('status == ' +status);
-    //          setTimeout(function() { // give it some time to load
-
-    //           _page.property('content').then(function(content) {
-    //             var gameArray = new Array ();
-    //             var $ = cheerio.load(content);
-
-    //             $('.scoreboard-wrapper').filter(function() {
-    //               var lineScore = $(this).find('.sb-linescore');
-    //               //console.log('lineScore == '+lineScore);
-
-    //               var line = $(this).find('.line').text();
-
-    //               var awayTeam = $(this).find('#teams').find('.away .sb-team-abbrev');
-    //               var homeTeam = $(this).find('#teams').find('.home .sb-team-abbrev');
-
-    //               var awayPitcher = $(this).find('#teams').find('.away .pitchers').find('a').attr('href');
-    //               var homePitcher = $(this).find('#teams').find('.home .pitchers').find('a').attr('href');
-
-    //               var awayPitcherID = '';
-    //               var homePitcherID = '';
-    //               if (awayPitcher) 
-    //               {
-    //                 awayPitcherID += awayPitcher.substring(awayPitcher.lastIndexOf("/") + 1, awayPitcher.length);
-    //                 homePitcherID += homePitcher.substring(homePitcher.lastIndexOf("/") + 1, homePitcher.length);
-    //               }
-
-    //               if (line.length > 1) 
-    //               {
-    //                   var gameObj = {
-    //                     'homeTeam': homeTeam.text(),
-    //                     'awayTeam': awayTeam.text(),
-    //                     'homePitcherID': homePitcherID,
-    //                     'awayPitcherID': awayPitcherID,
-    //                     'line': line
-    //                   };
-    //                   gameArray.push(gameObj);
-    //               }
-
-    //             });
-    //             // var bracketData = JSON.stringify(json, null, 4);
-    //             // callback(bracketData);
-
-    //             page.close();
-    //             ph.exit();
-    //             //var gameData = JSON.stringify(json);
-    //             return callback(null, gameArray);
-    //             // console.log('Product json == '+JSON.stringify(json));
-    //           });
-
-    //           // _page.evaluate(function() {
-    //           // });
-
-
-    //          }, 5000);
-    //     });
-    //   });
-    // });
+      .catch(e => console.log(e));
 }
 
 
--- routes/api.js
+++ routes/api.js
@@ -26,48 +26,16 @@ module.exports = function(app) {
   app.post('/scrape',function(req,res) {
 
     MLB.getAnalyticsForAdvantagePlayer(function(err, gameData) {
-      //console.log('Product json == '+gameData);
       console.log(err)
-      var jsonGameData = JSON.stringify(gameData);
       var jsonParseGameData = JSON.parse(JSON.stringify(gameData));
-      const fields = ['homeTeam', 'awayTeam', 'homePitcherID', 'awayPitcherID', 'line', 'overUnder'];
-      const json2csvParser = new Parser({ fields });
-      const csv = json2csvParser.parse(jsonParseGameData);
+      var fields = ['homeTeam', 'awayTeam', 'homePitcherID', 'awayPitcherID', 'line', 'overUnder'];
+      var json2csvParser = new Parser({ fields });
+      var csv = json2csvParser.parse(jsonParseGameData);
 
-
-      console.log('Product json == '+jsonGameData);
-      console.log('Product csv == '+csv);
-      // res.end(gameData);
+      // console.log('Product json == '+JSON.stringify(gameData));
+      // console.log('Product csv == '+csv);
       return res.json({'csv':csv});
     });
-
-
-    //const url = 'https://sports.yahoo.com/mlb/scoreboard/';
-
-    // phantom.create().then(function(ph) {
-    //   ph.createPage().then(function(page) {
-    //     page.property('customHeaders', {'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4'})
-    //     page.open(url).then(function(status) {
-    //       console.log('status == ' +status);
-    //          setTimeout(function() { // give it some time to load
-    //           page.property('content').then(function(content) {
-    //             var $ = cheerio.load(content),
-    //                     data = content,
-    //                     json = [];
-
-    //             //console.log('content == '+content);
-
-    //             $('#scoreboard-group-2 ul li').filter(function() {
-    //               console.log('scoreboard group == '+$(this));
-    //             });
-    //           });
-    //             page.close();
-    //             ph.exit();
-    //          }, 5000);
-    //     });
-    //   });
-    // });
-
 
   });
 
